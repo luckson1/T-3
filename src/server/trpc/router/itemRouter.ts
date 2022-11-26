f53eadc97b7e30441659fd5a297e220cc3ad7335
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-11-26 21:33:35 +0300

    fix: fix data fetching

--- src/server/trpc/router/itemRouter.ts
+++ src/server/trpc/router/itemRouter.ts
@@ -1,39 +1,59 @@
 import { z } from "zod";
 import { publicProcedure, router } from "../trpc";
 
-export const itemRouter= router({
-    getShoppingItem:  publicProcedure.query(async ({ ctx}) => {
-        const items= await ctx.prisma.shoppingItem.findMany();
-        return items
-      }),
-      createShoppingItem: publicProcedure.input(
-        z.object({
-            name: z.string()
-        })
-      )
-      .mutation( async ({input, ctx})=> {
-        const {name}=input
-       const item= await ctx.prisma.shoppingItem.create({
-            data: {
-                name,
-                checked: false
-            }
-        })
-        return item
-      }),
-      deleteShoppingItem:  publicProcedure.input(
-        z.object({
-            id: z.string()
-        })
-      )
-      .mutation( async ({input, ctx})=> {
-        const {id}=input
-       const item= await ctx.prisma.shoppingItem.delete({
-            where: {
-               id
-            }
-        })
-        return item
-      }),
-    }
-      )
+export const itemRouter = router({
+  getShoppingItem: publicProcedure.query(async ({ ctx }) => {
+    const items = await ctx.prisma.shoppingItem.findMany();
+    return items;
+  }),
+  createShoppingItem: publicProcedure
+    .input(
+      z.object({
+        name: z.string(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { name } = input;
+      const item = await ctx.prisma.shoppingItem.create({
+        data: {
+          name,
+          checked: false,
+        },
+      });
+      return item;
+    }),
+  deleteShoppingItem: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { id } = input;
+      const item = await ctx.prisma.shoppingItem.delete({
+        where: {
+          id,
+        },
+      });
+      return item;
+    }),
+  checkShoppingItem: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        checked: z.boolean(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { id, checked } = input;
+      const item = await ctx.prisma.shoppingItem.update({
+        where: {
+          id,
+        },
+        data: {
+          checked,
+        },
+      });
+      return item;
+    }),
+});
